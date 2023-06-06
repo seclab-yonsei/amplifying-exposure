8c9b67137b8bf94c61008b1570cf2575be945416
myunggyo <cawandmilk@gmail.com>
2023-06-06 11:29:55 +0000

    Update files

--- nsml_run.sh
+++ nsml_run.sh
@@ -1,6 +1,8 @@
 ## Get some private information from arguments.
 WANDB_API_KEY=$1
 
+#!/bin/bash
+
 ## Copy the github repository.
 cp -rf /mnt/prj/mrt /mnt/block-storage/mrt
 
@@ -8,16 +10,13 @@ cp -rf /mnt/prj/mrt /mnt/block-storage/mrt
 ## See: https://guide.ncloud-docs.com/docs/ai-clova-nsml-1-3
 mkdir -p /mnt/block-storeage/$NSML_RUN/ckpt
 
-## Update.
-sudo apt-get update
-
 ## Install all requirements in local. (i.e., not 'conda env' or 'venv', ...)
 cd /mnt/block-storage/mrt
 
 pip install --upgrade pip
 pip install torch transformers lightning easydict black wandb
 pip install transformers[deepspeed]
-sudo apt install libaio-dev
+# sudo apt install -y libaio-dev
 
 ds_report
 wandb login $WANDB_API_KEY
@@ -33,3 +32,6 @@ ln -s /mnt/block-storage/$NSML_RUN/ckpt ckpt
 ## Train and record all outputs (stdout, stderr) to a log file.
 deepspeed --num_gpus=2 train.py --deepspeed ./assets/ds_config_zero3.json \
   > /mnt/block-storeage/$NSML_RUN/log.log 2>&1
+
+## Return.
+exit 0
