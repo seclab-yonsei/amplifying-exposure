834af573e3b02ecf44fa23c2f97f33cfcfa0232e
myunggyo <cawandmilk@gmail.com>
2023-06-24 20:50:43 +0900

    Update files

--- src/rl_lightning.py
+++ src/rl_lightning.py
@@ -1,9 +1,12 @@
 import torch
 import lightning as L
 
+import tqdm
+
 import numpy as np
 
 from deepspeed.ops.adam import DeepSpeedCPUAdam
+from operator import itemgetter
 
 from src.score import GPTScorer
 
@@ -17,20 +20,20 @@ class MinimumRiskTrainingModule(L.LightningModule):
         self.config = config
 
         self.score_fn = GPTScorer(tok=tok)
-        self.total_batch_size = (
-            self.config.devices
-            * self.config.batch_size
-            * self.config.accumulate_grad_batches
-        )
-        self.total_steps = int(
-            np.ceil(
-                self.config.samples_per_epoch
-                * self.config.max_epochs
-                / self.total_batch_size
-            )
-        )
         self.alpha = 0.017
-        self.replay_buffer = []
+
+        ## Auto-parameters.
+        self.per_replica_batch_size = (
+            self.config.batch_size * self.config.accumulate_grad_batches
+        )
+        self.global_batch_size = (
+            self.per_replica_batch_size * self.config.devices
+        )
+        self.total_steps = (
+            int(np.ceil(self.config.samples_per_epoch / self.global_batch_size))
+            * self.config.max_epochs
+        )
+        self.replay_buffer = None  ## per device
 
         ## Save hyper-parameters to self.hparams (auto-logged by W&B).
         self.save_hyperparameters(ignore=["model"])
@@ -54,7 +57,7 @@ class MinimumRiskTrainingModule(L.LightningModule):
     def forward(self, *args, **kwargs):
         return self.model(*args, **kwargs)
 
-    def configure_optimizers(self):
+    def configure_optimizers(self) -> dict:
         optimizer = DeepSpeedCPUAdam(self.parameters(), lr=self.config.lr)
         scheduler = torch.optim.lr_scheduler.OneCycleLR(
             optimizer,
@@ -70,50 +73,80 @@ class MinimumRiskTrainingModule(L.LightningModule):
             },
         }
 
-    """
-    def on_train_epoch_start(self):
-        input_ids = (
-            torch.tensor([self.tok.eos_token_id], dtype=torch.int32)
-            .repeat(self.config.batch_size)
-            .to(self.model.device)
-        )
-        prompt_len = input_ids.size(1)
+    @torch.inference_mode()
+    def on_train_epoch_start(self) -> None:
+        ## Make a prompt.
+        prompt = [self.tok.eos_token_id]
+        input_ids = torch.LongTensor(prompt).to(self.model.device)
 
-        buffer = []
-        n_iter = int(
-            np.ceil(
-                self.config.buffer_size
-                / (self.config.batch_size * self.config.devices)
-            )
+        prompt_len = input_ids.size(0)  ## not 1
+        assert prompt_len == 1
+
+        ## Prepare a local buffer (per device).
+        buf = []
+
+        ## Get ready to start `tqdm`.
+        iterable = range(0, self.config.buffer_size, self.config.batch_size)
+        desc = f"Preparing a replay buffer ({self.model.device})"
+        position = (
+            int(str(self.model.device).split(":")[-1])
+            if str(self.model.device).startswith("cuda")
+            else 0
         )
-        for i in tqdm.tqdm(range(n_iter), desc="Preparing the replay buffer"):
-            bs = i * self.config.batch_size
+        for i in tqdm.tqdm(iterable, desc=desc, position=position):
+            bs = (  ## more batch size can be allowed in inference mode
+                self.config.batch_size
+                if i + self.config.batch_size < self.config.buffer_size
+                else self.config.buffer_size - i
+            )
             gen_tokens = (
                 self.model.generate(
-                    input_ids,
+                    input_ids.repeat(bs, 1),
                     do_sample=self.config.do_sample,
                     min_length=self.config.min_new_tokens + prompt_len,
                     max_length=self.config.max_new_tokens + prompt_len,
                     no_repeat_ngram_size=self.config.no_repeat_ngram_size,
                     top_p=self.config.top_p,
                     top_k=self.config.top_k,
-                    synced_gpus=True,
+                    # synced_gpus=True,
                 )
                 .clone()
                 .detach()
             )
             rewards = self._get_reward(gen_tokens)
-            for ii in range(gen_tokens.size(0)):
-                buffer.append(
-                    {
-                        "y": gen_tokens[ii],
-                        "reward": rewards[ii],
-                    }
-                )
+
+            ## Stack.
+            buf += [{"y": g, "reward": r} for g, r in zip(gen_tokens, rewards)]
 
         ## Extend our buffer.
-        self.replay_buffer += buffer
-    """
+        assert self.replay_buffer == None
+        self.replay_buffer = buf
+        assert (
+            len(self.replay_buffer) == self.config.buffer_size
+        ), f"{len(self.replay_buffer)} != {self.config.buffer_size}"
+
+    @torch.inference_mode()
+    def fetch_y_from_replay_buffer(self) -> tuple:
+        ## Sort buffer by ascending order.
+        self.replay_buffer = sorted(
+            self.replay_buffer, key=itemgetter("reward")
+        )
+        element = self.replay_buffer[-1]
+
+        return (element["y"], element["reward"])
+
+    @torch.inference_mode()
+    def insert_y_hat_to_replay_buffer(
+        self, y_hat: torch.Tensor, y_hat_reward: torch.Tensor
+    ) -> None:
+        ## Insert items.
+        buf = [{"y": g, "reward": r} for g, r in zip(y_hat, y_hat_reward)]
+        self.replay_buffer += buf
+
+        ## Select top-k items.
+        self.replay_buffer = sorted(
+            self.replay_buffer, key=itemgetter("reward"), reverse=True
+        )[: self.config.buffer_size]
 
     def training_step(self, batch: dict, batch_idx) -> dict:
         ## Maximum likelihood.
@@ -122,6 +155,10 @@ class MinimumRiskTrainingModule(L.LightningModule):
 
         prompt_len = x.size(1)
         assert prompt_len == 1
+
+        ## Fetch pseudy y.
+        y, y_reward = self.fetch_y_from_replay_buffer()
+        y_reward.to(self.model.device)
 
         ## Sample y_hat, logits, and the loss.
         y_hat: torch.Tensor = self.model.generate(
@@ -172,8 +209,7 @@ class MinimumRiskTrainingModule(L.LightningModule):
 
             ## Now, we have relatively expected cumulative reward.
             ## Which score can be drawn from actor_reward substracted by baseline.
-            reward = actor_reward - baseline
-            # reward = (actor_reward - baseline) / torch.max(actor_reward) * 100.0
+            reward = (actor_reward - baseline) / y_reward * 100.0
             ## |reward| = (batch_size,)
 
         ## Calculate minimum risk training loss.
@@ -182,6 +218,9 @@ class MinimumRiskTrainingModule(L.LightningModule):
 
         loss = self.alpha * ce_loss + (1 - self.alpha) * rl_loss  ## alpha=0.1
         ## |loss| = (1,)
+
+        ## Insert y_hat and their rewards.
+        self.insert_y_hat_to_replay_buffer(y_hat.clone().detach(), actor_reward)
 
         ## Make a return dict.
         metrics = {
